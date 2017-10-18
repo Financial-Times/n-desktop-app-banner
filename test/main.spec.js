32c708f67de10bec96658fa3fa3ca97cc56c451b
Remy Bach <remy@bach.me.uk>
2017-10-18 16:23:56 +0100

    Hide on mobile and clean up tracking code  🐿 v2.5.16

--- test/main.spec.js
+++ test/main.spec.js
@@ -4,102 +4,133 @@ import fetchMock from 'fetch-mock';
 
 import DesktopAppBanner from '../main.js';
 
+DesktopAppBanner.prototype.trackEvent = sinon.spy();
+
 let desktopAppBanner;
 
 describe('DesktopAppBanner', () => {
 
 	const initialTemplate = document.querySelector('.js-n-app-banner-wrapper').outerHTML;
 
-	beforeEach(() => {
-		desktopAppBanner = new DesktopAppBanner();
-	});
+	context('is showable', () => {
+		beforeEach(() => {
+			DesktopAppBanner.prototype.isShowable = () => true;
 
-	afterEach(() => {
-	  document.querySelector('.js-n-app-banner-wrapper').outerHTML = initialTemplate;
-	});
-
-	it('should be able to grab the dom elements it needs.', () => {
-		expect(desktopAppBanner.closeLink).not.to.be.undefined;
-		expect(desktopAppBanner.emailButton).not.to.be.undefined;
-		expect(desktopAppBanner.errorMessage).not.to.be.undefined;
-		expect(desktopAppBanner.form).not.to.be.undefined;
-		expect(desktopAppBanner.wrapper).not.to.be.undefined;
-	});
-
-	context('event binding', () => {
-		it('binds on the close link', () => {
-			sinon.spy(desktopAppBanner.closeLink, 'addEventListener');
-
-			desktopAppBanner.bindEvents();
-
-			expect(desktopAppBanner.closeLink.addEventListener.getCall(0).args[0]).to.eql('click');
-
-			desktopAppBanner.closeLink.addEventListener.reset();
+			desktopAppBanner = new DesktopAppBanner();
 		});
-
-		it('binds on form submit', () => {
-			sinon.spy(desktopAppBanner.form, 'addEventListener');
-
-			desktopAppBanner.bindEvents();
-
-			expect(desktopAppBanner.form.addEventListener.getCall(0).args[0]).to.eql('submit');
-
-			desktopAppBanner.form.addEventListener.reset();
-		});
-	});
-
-	it('hides the banner when clicking on the close link', () => {
-    let clickEvent = new MouseEvent('click', {
-        'view': window,
-        'bubbles': false,
-        'cancelable': true
-      });
-
-		desktopAppBanner.closeLink.dispatchEvent(clickEvent);
-
-		expect(desktopAppBanner.wrapper.style).to.have.property('bottom');
-	});
-
-	context('making the call to send the email', () => {
 
 		afterEach(() => {
-			fetchMock.restore();
+			document.querySelector('.js-n-app-banner-wrapper').outerHTML = initialTemplate;
+			DesktopAppBanner.prototype.trackEvent.reset();
 		});
 
-		it('makes the call to send the email on form submit and disables the submit button', () => {
-			fetchMock.post(desktopAppBanner.form.action, 200);
-			let fetchSpy = sinon.spy(window, 'fetch');
-
-			// We're calling this directly as submitting the actual form makes the page redirect and messes up the tests.
-			desktopAppBanner.handleFormSubmit({ preventDefault: () => {} });
-
-			sinon.assert.calledOnce(fetchSpy);
-			expect(desktopAppBanner.emailButton.disabled).to.be.true;
-
-			window.fetch.restore();
-
+		it('should be able to grab the dom elements it needs.', () => {
+			expect(desktopAppBanner.closeLink).not.to.be.undefined;
+			expect(desktopAppBanner.emailButton).not.to.be.undefined;
+			expect(desktopAppBanner.errorMessage).not.to.be.undefined;
+			expect(desktopAppBanner.form).not.to.be.undefined;
+			expect(desktopAppBanner.wrapper).not.to.be.undefined;
 		});
 
-		it('shows a thank you message if successful', done => {
-			fetchMock.post(desktopAppBanner.form.action, 200);
+		context('event binding', () => {
+			it('binds on the close link', () => {
+				sinon.spy(desktopAppBanner.closeLink, 'addEventListener');
 
-			desktopAppBanner.submitForm().then(() => {
-				expect(desktopAppBanner.wrapper.className).to.have.string('has-sent');
+				desktopAppBanner.bindEvents();
 
-				done();
+				expect(desktopAppBanner.closeLink.addEventListener.getCall(0).args[0]).to.eql('click');
+
+				desktopAppBanner.closeLink.addEventListener.reset();
+			});
+
+			it('binds on form submit', () => {
+				sinon.spy(desktopAppBanner.form, 'addEventListener');
+
+				desktopAppBanner.bindEvents();
+
+				expect(desktopAppBanner.form.addEventListener.getCall(0).args[0]).to.eql('submit');
+
+				desktopAppBanner.form.addEventListener.reset();
 			});
 		});
 
-		it('shows an error message if failure and re-enables the submit button', done => {
-			fetchMock.post(desktopAppBanner.form.action, 500);
+		it('tracks when the component is viewed', () => {
+			expect(desktopAppBanner.trackEvent.getCall(0).args[0].action).to.eql('view');
+		});
 
-			desktopAppBanner.submitForm().then(() => {
-				expect(desktopAppBanner.errorMessage.innerHTML).to.eql('<p><strong>Oops!</strong> Please try again.</p>');
-				expect(desktopAppBanner.emailButton.disabled).to.be.false;
+		it('hides the banner when clicking on the close link', () => {
+			let clickEvent = new MouseEvent('click', {
+					'view': window,
+					'bubbles': false,
+					'cancelable': true
+				});
 
-				done();
+			desktopAppBanner.closeLink.dispatchEvent(clickEvent);
+
+			expect(desktopAppBanner.wrapper.style).to.have.property('bottom');
+			expect(desktopAppBanner.trackEvent.getCall(1).args[0].action).to.eql('close');
+		});
+
+		context('making the call to send the email', () => {
+
+			afterEach(() => {
+				fetchMock.restore();
+			});
+
+			it('makes the call to send the email on form submit and disables the submit button', () => {
+				fetchMock.post(desktopAppBanner.form.action, 200);
+				let fetchSpy = sinon.spy(window, 'fetch');
+
+				// We're calling this directly as submitting the actual form makes the page redirect and messes up the tests.
+				desktopAppBanner.handleFormSubmit({ preventDefault: () => {} });
+
+				sinon.assert.calledOnce(fetchSpy);
+				expect(desktopAppBanner.emailButton.disabled).to.be.true;
+				expect(desktopAppBanner.trackEvent.getCall(1).args[0].action).to.eql('act');
+
+				window.fetch.restore();
+
+			});
+
+			it('shows a thank you message if successful', done => {
+				fetchMock.post(desktopAppBanner.form.action, 200);
+
+				desktopAppBanner.submitForm().then(() => {
+					expect(desktopAppBanner.wrapper.className).to.have.string('has-sent');
+
+					done();
+				});
+			});
+
+			it('shows an error message if failure and re-enables the submit button', done => {
+				fetchMock.post(desktopAppBanner.form.action, 500);
+
+				desktopAppBanner.submitForm().then(() => {
+					expect(desktopAppBanner.errorMessage.innerHTML).to.eql('<p><strong>Oops!</strong> Please try again.</p>');
+					expect(desktopAppBanner.emailButton.disabled).to.be.false;
+
+					done();
+				});
 			});
 		});
+	});
+
+	context('is NOT showable', () => {
+		beforeEach(() => {
+			DesktopAppBanner.prototype.isShowable = () => false;
+
+			desktopAppBanner = new DesktopAppBanner();
+		});
+
+		afterEach(() => {
+			DesktopAppBanner.prototype.trackEvent.reset();
+		});
+
+		it('removes the markup and fires a skip event', () => {
+			expect(document.querySelector('.js-n-app-banner-wrapper')).to.be.null;
+			expect(desktopAppBanner.trackEvent.getCall(0).args[0].action).to.eql('skip');
+		});
+
 	});
 
 });
